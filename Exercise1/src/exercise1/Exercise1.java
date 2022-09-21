bd523ae96d68da7c48960e920ec3aa49108b5b37
Is örn <manuel.isorna@iesnervion.es>
2022-09-21 13:16:34 +0200

    First commit.

---
+++ Exercise1/src/exercise1/Exercise1.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		short number;
+
+		System.out.print("Introduzca un número: ");
+		number = sc.nextShort();
+		System.out.println("The number is " + number);
+	}
+}
